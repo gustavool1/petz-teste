d8b3d181b224479121227e309d7e8e5ea50ee394
gustavool1 <go8895806@gmail.com>
2023-10-16 18:21:47 -0300

    feat: adding global style to the app

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,4 +1,4 @@
-import GlobalStyle from "../services/globalStyles";
+import GlobalStyle from "../helpers/globalStyles";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {

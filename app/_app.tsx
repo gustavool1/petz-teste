5ce07dd8b4e68ef07ce8f432113781dc0de50720
gustavool1 <go8895806@gmail.com>
2023-10-16 18:21:20 -0300

    feat: changing the directory pattern

---
+++ app/_app.tsx
@@ -0,0 +1,11 @@
+import GlobalStyle from "../services/globalStyles";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}

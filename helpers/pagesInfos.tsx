a391e8e895e80ad15f7eac4a9172ff8443fcdc3f
gustavool1 <go8895806@gmail.com>
2023-10-17 17:17:12 -0300

    feat: creating helpers

---
+++ helpers/pagesInfos.tsx
@@ -0,0 +1,18 @@
+interface AboutPage {
+  path: string
+  title: string
+  description: string
+}
+
+export const pagesInfos: AboutPage[] = [
+  {
+    path: 'about',
+    description: 'A maior rede de tratamento pokémon.',
+    title: 'Quem Somos',
+  },
+  {
+    path: 'schedule',
+    description: 'Recupere seus pokémons em 5 segundos',
+    title: 'Agendar Consulta',
+  },
+]

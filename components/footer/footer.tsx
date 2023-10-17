1069b2539d5673d3f5159eb84a4d8b3162dcc59d
gustavool1 <go8895806@gmail.com>
2023-10-17 10:45:11 -0300

    feat: creating the footer component

---
+++ components/footer/footer.tsx
@@ -0,0 +1,13 @@
+import { Container, Title } from './style'
+
+const Footer = () => {
+  return (
+    <Container>
+      <Title>
+        Todas as marcas e ilustrações utilizadas são de seus resepctivos donos.
+      </Title>
+    </Container>
+  )
+}
+
+export default Footer
---
+++ components/footer/style.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 72px;
+  width: 100%;
+  background-color: var(--color-secondary);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Title = styled.h2`
+  font-size: 14px;
+  color: white;
+  font-weight: 400;
+`

991b31b6d0b3e4ddd6a3515267d76b4724935999
gustavool1 <go8895806@gmail.com>
2023-10-17 17:18:49 -0300

    feat: exporting components

--- components/components.tsx
+++ components/components.tsx
@@ -1,4 +1,6 @@
+import Button from './button/button'
 import Footer from './footer/footer'
 import NavBar from './navbar/navbar'
+import NavigationHeader from './navigationHeader/navigationHeader'
 
-export { NavBar, Footer }
+export { NavBar, Footer, Button, NavigationHeader }

f9d5ea6567a41e3e9a5c87edabd8e25e9eccf7a4
gustavool1 <go8895806@gmail.com>
2023-10-17 17:17:49 -0300

    feat: creating the wireframe of the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,40 @@
+'use client'
 import React from 'react'
+import {
+  Container,
+  MessageContainer,
+  PokemonCenterHeader,
+  PokemonMessage,
+} from './style'
+import { NavigationHeader, NavBar, Footer } from '../../components/components'
+import {
+  bestForYourPokemon,
+  healPokemon,
+  highTech,
+  tradition,
+} from '../../helpers/loremIpsum'
 
 const AboutPage = () => {
-  return <div>About</div>
+  return (
+    <Container>
+      <NavBar />
+      <NavigationHeader />
+      <MessageContainer>
+        <PokemonCenterHeader>O Centro Pokémon</PokemonCenterHeader>
+        <PokemonMessage>
+          O Centro Pokémon Como funciona a cura de um pokémon?
+        </PokemonMessage>
+        <PokemonMessage>{healPokemon}</PokemonMessage>
+        <PokemonMessage>Uma tradição de mais de 20 anos</PokemonMessage>
+        <PokemonMessage>{tradition}</PokemonMessage>
+        <PokemonMessage>O melhor para seu pokémon</PokemonMessage>
+        <PokemonMessage>{bestForYourPokemon}</PokemonMessage>
+        <PokemonMessage>Alta Tecnologia</PokemonMessage>
+        <PokemonMessage>{highTech}</PokemonMessage>
+      </MessageContainer>
+      <Footer />
+    </Container>
+  )
 }
 
 export default AboutPage
---
+++ app/about/style.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Container = styled.div``
+
+export const MessageContainer = styled.div`
+  max-width: 400px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  margin-top: 20px;
+`
+export const PokemonCenterHeader = styled.h1`
+  font-size: 18px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`
+export const PokemonMessage = styled.h1`
+  font-size: 16px;
+  font-weight: 500;
+  margin-bottom: 20px;
+`

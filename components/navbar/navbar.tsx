1f961b8e01a01c6da2143bc7804dae747fe8b051
gustavool1 <go8895806@gmail.com>
2023-10-17 15:17:02 -0300

    feat: create basic style for navbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,7 +1,36 @@
-import { Container } from './style'
+import Link from 'next/link'
+import Button from '../button/button'
+import {
+  Container,
+  ButtonTextPokemonCenter,
+  CentralPokemonContainer,
+  ButtonTextSchedule,
+  InfoContainer,
+  WhoWeAre,
+} from './style'
+
+import { useRouter } from 'next/navigation'
 
 const NavBar = () => {
-  return <Container>Navbar</Container>
+  const router = useRouter()
+
+  return (
+    <Container>
+      <CentralPokemonContainer>
+        <Button hasIcon onClick={() => {}}>
+          <ButtonTextPokemonCenter>Centro Pokémon</ButtonTextPokemonCenter>
+        </Button>
+      </CentralPokemonContainer>
+      <InfoContainer>
+        <WhoWeAre onClick={() => router.push('/about')}>Quem Somos</WhoWeAre>
+        <Button>
+          <ButtonTextSchedule onClick={() => router.push('/schedule')}>
+            Agendar Consulta
+          </ButtonTextSchedule>
+        </Button>
+      </InfoContainer>
+    </Container>
+  )
 }
 
 export default NavBar
--- components/navbar/style.tsx
+++ components/navbar/style.tsx
@@ -4,4 +4,41 @@ export const Container = styled.div`
   height: 104px;
   width: 100%;
   background-color: white;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+`
+export const CentralPokemonContainer = styled.div`
+  width: 50%;
+  margin-left: 65px;
+`
+export const ButtonTextPokemonCenter = styled.h2`
+  font-size: 20;
+  font-weight: 600;
+  color: white;
+`
+
+export const ButtonTextSchedule = styled.h2`
+  font-size: 14px;
+  font-weight: 700;
+  color: white;
+`
+
+export const InfoContainer = styled.div`
+  width: 50%;
+  display: flex;
+  margin-right: 65px;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+`
+export const WhoWeAre = styled.button`
+  font-size: 14px;
+  font-weight: 400;
+  color: black;
+  margin-right: 30px;
+  padding: 20px;
+  cursor: pointer;
+  border: none;
+  background-color: transparent;
 `

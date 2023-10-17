e65ed569d3f134126621448043b56afdd5f3efe0
gustavool1 <go8895806@gmail.com>
2023-10-17 17:18:02 -0300

    feat: creating the wireframe of the home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+'use client'
+import React from 'react'
+import { CenteredText, Container, ImageContainer, Title } from './style'
+import Image from 'next/image'
+import { NavBar, Footer, NavigationHeader } from '../../components/components'
 
-const HomePage = () => {
-  return <div>This is the Home Page.</div>;
-};
-
-export default HomePage;
+export default function HomePage() {
+  return (
+    <Container>
+      <NavBar />
+      <ImageContainer>
+        <Image
+          alt="Background Image"
+          src="/images/pokemon-hero.jpg"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: '100%', height: '100%' }}
+        />
+        <CenteredText>
+          <Title>
+            Cuidamos bem do seu pokémon, para ele cuidar bem de você
+          </Title>
+        </CenteredText>
+      </ImageContainer>
+      <Footer />
+    </Container>
+  )
+}
---
+++ app/home/style.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  padding: 0px;
+  width: 100%;
+  height: calc(100vh - 176px);
+`
+
+export const ImageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`
+export const CenteredText = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  max-width: 510px;
+`
+
+export const Title = styled.h1`
+  color: white;
+  font-size: 32px;
+  font-weigth: 700;
+`

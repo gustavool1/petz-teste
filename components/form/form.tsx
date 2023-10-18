86d35d363279bb3d512d327863aaedd188db95e5
gustavool1 <go8895806@gmail.com>
2023-10-18 16:47:04 -0300

    feat: creating basic style of the form

--- components/form/form.tsx
+++ components/form/form.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import UserInfo from './components/userInfo/userInfo'
 import { Container, Title } from './style'
-import RegionInfo from './components/location/location'
+import LocationInfo from './components/location/location'
 import RegisterTeam from './components/registerTeam/registerTeam'
+import ButtonSecondary from '../buttonSecondary/buttonPrimary'
 
 const Form = () => {
   return (
     <Container>
       <Title>Preencha o formulário abaixo para agendar sua consulta</Title>
       <UserInfo />
-      <RegionInfo />
+      <LocationInfo />
       <RegisterTeam />
     </Container>
   )
---
+++ components/form/style.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  flex-direction: column;
+  flex-wrap: wrap;
+  max-width: 652px;
+  margin: 0 auto;
+`
+export const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 600;
+  margin-top: 30px;
+`

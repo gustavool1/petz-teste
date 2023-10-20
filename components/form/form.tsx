8e571f88317c1435cd7ba6154bbba0afc5d54247
gustavool1 <go8895806@gmail.com>
2023-10-20 12:18:33 -0300

    feat: calling modal on the scheudle page

--- components/form/form.tsx
+++ components/form/form.tsx
@@ -7,14 +7,29 @@ import ButtonSecondary from '../buttonSecondary/buttonSecondary'
 import Date from './components/date/date'
 import FinancialResume from './components/financialResume/financialResume'
 import { useSchedule } from '../../hooks/schedule'
+import { Modal } from '../components'
+import { ModalType } from '../modal/modal'
+
+export interface ModalInfos {
+  isShowing: boolean
+  type: ModalType
+  pokemonQuantity?: string
+  day?: string
+  time?: string
+}
 
 const Form = () => {
   const [pokemonTeam, setPokemonTeam] = useState(1)
+  const [modalInfos, setModalInfos] = useState<ModalInfos>({
+    isShowing: false,
+    type: ModalType.success,
+  })
   const { handleSubmit, onSubmit, register, errors } = useSchedule()
+
   return (
     <Container>
       <Title>Preencha o formulário abaixo para agendar sua consulta</Title>
-      <FormSchedule onSubmit={handleSubmit(onSubmit)}>
+      <FormSchedule onSubmit={handleSubmit(onSubmit(setModalInfos))}>
         <UserInfo register={register} errors={errors} />
         <LocationInfo register={register} errors={errors} />
         <RegisterTeam
@@ -25,11 +40,24 @@ const Form = () => {
         <ButtonSecondary
           onClick={() => setPokemonTeam(pokemonTeam + 1)}
           hasIcon
+          type="button"
+          disabled={!(pokemonTeam <= 5)}
         >
           <AddPokemon>Adicionar novo pokémon ao time...</AddPokemon>
         </ButtonSecondary>
         <Date register={register} errors={errors} />
-        <FinancialResume />
+        <FinancialResume pokemonTeam={pokemonTeam} />
+        {modalInfos.isShowing && (
+          <Modal
+            setIsShowingModal={() => {
+              setModalInfos({ ...modalInfos, isShowing: false })
+            }}
+            type={modalInfos.type}
+            day={modalInfos.day}
+            pokemonQuantity={modalInfos.pokemonQuantity}
+            time={modalInfos.time}
+          />
+        )}
       </FormSchedule>
     </Container>
   )

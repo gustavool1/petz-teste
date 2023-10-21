4c907150e7b77bb1bb783a6f17027e73811c04c9
gustavool1 <go8895806@gmail.com>
2023-10-21 14:27:14 -0300

    feat: getting the api url from .evn

--- helpers/apis.tsx
+++ helpers/apis.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: process.env.NEXT_PUBLIC_URL,
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
